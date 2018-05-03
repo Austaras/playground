8fbc8d47b0f2d0b88b093a58b4dcfc37be6f01a8
austaras <austaras@outlook.com>
2018-05-03 23:30:43 +0800

    modify

--- auto-unsub/index.ts
+++ auto-unsub/index.ts
@@ -1,24 +1,12 @@
 import { Component } from "./component"
 import { BasicService } from "./service"
-import { init } from "./utils";
+import { mount, unmount } from "./utils";
 
 const services: Services = { 'basic': new BasicService() } // Init services
 
-const component = init(Component, services)//new Component(services.ser) // Init components
-
-function umount(comp: Warpped) {
-    if (comp.watch){
-        for (const [key, val] of Object.entries(comp.watch)) {
-            val.forEach((f: Function) => {
-                services[key].unsub(f)
-            })
-        }
-    }
-
-    comp = {}
-}
+const component = mount(Component, services)//new Component(services.ser) // Init components
 
 services.basic.pub()  // serive publish message
-umount(component)  // component unmounted
+unmount(component, services)
 services.basic.sub(() => console.log("subscribing by index"))
 services.basic.pub()
--- auto-unsub/utils.ts
+++ auto-unsub/utils.ts
@@ -19,7 +19,7 @@ export function trace(obj: any, watch: Function[]) {
     return new Proxy(obj, handler)
 }
 
-export function init(constructor: { new(...args: any[]): {}}, ser: Services): Warpped {
+export function mount(constructor: { new(...args: any[]): {}}, ser: Services): Warpped {
     const param = Reflect.getMetadata("design:paramtypes", constructor)
     const data: any[] = new Array(param.length)
     const watch: any = {}
@@ -33,6 +33,18 @@ export function init(constructor: { new(...args: any[]): {}}, ser: Services): Wa
     const comp : any = new constructor(...data)
     comp.watch = watch
     return comp
+}
+
+export function unmount(comp: Warpped, services: Services) {
+    if (comp.watch){
+        for (const [key, val] of Object.entries(comp.watch)) {
+            val.forEach((f: Function) => {
+                services[key].unsub(f)
+            })
+        }
+    }
+
+    comp = {}
 }
 
 export function Inject(constructor: Function) { }
