1203b3033ef1c0255011cf9302fd1b86ee518cbb
austaras <austaras@outlook.com>
2018-09-05 20:18:55 +0800

    polish

--- rxjs/count/index.ts
+++ rxjs/count/index.ts
@@ -1,20 +1,19 @@
-import { fromEvent } from 'rxjs'
-import { mapTo, merge, scan } from 'rxjs/operators'
+import { fromEvent, merge } from 'rxjs'
+import { mapTo, scan, startWith } from 'rxjs/operators'
 
 const add = document.querySelector('#add') as HTMLButtonElement
 const minus = document.querySelector('#minus') as HTMLButtonElement
 const text = document.querySelector('#text') as HTMLParagraphElement
 
-render(0)
-
 const add$ = fromEvent(add, 'click')
 const minus$ = fromEvent(minus, 'click')
 
-add$.pipe(
-    mapTo(1),
-    merge(minus$.pipe(mapTo(-1))),
-    scan((acc, val) => acc += val, 0)
-).subscribe(val => render(val))
+merge(add$.pipe(mapTo(1)), minus$.pipe(mapTo(-1)))
+    .pipe(
+        startWith(0),
+        scan((acc, val) => acc += val)
+    )
+    .subscribe(val => render(val))
 
 function render(count: number) {
     text.innerText = `Count currently is ${count}`
