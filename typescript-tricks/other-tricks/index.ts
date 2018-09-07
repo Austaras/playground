e0d17999b362546c01f1ec7ea76366c69140f96f
austaras <austaras@outlook.com>
2018-09-07 20:54:13 +0800

    beautify index

--- typescript-tricks/other-tricks/index.ts
+++ typescript-tricks/other-tricks/index.ts
@@ -1,13 +1,3 @@
-function timeoutPromise(time: number) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(resolve, time)
-    })
-}
-
-console.log(performance.now())
-timeoutPromise(1000)
-    .then(() => console.log(performance.now()))
-
 function warn() {
     console.warn('need argument')
 }
