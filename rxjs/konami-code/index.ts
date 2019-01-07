dc2dba1dc0220f9da5f22a0f1355627865ebff5d
austaras <austaras@outlook.com>
2019-01-07 21:01:11 +0800

    fix a bug sometimes first few key is truncated by windowCount

--- rxjs/konami-code/index.ts
+++ rxjs/konami-code/index.ts
@@ -1,5 +1,5 @@
 import { fromEvent, Observable, timer } from 'rxjs'
-import { map, mergeAll, reduce, takeUntil, windowCount } from 'rxjs/operators'
+import { map, mergeAll, reduce, takeUntil, windowCount, tap, share, mergeMap, take } from 'rxjs/operators'
 
 const PATTERN = [
     'ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown',
@@ -19,19 +19,22 @@ function arrayEqual(a: any[], b: any[]) {
 const timer$ = timer(TIME_LIMIT_MS)
 const ele = document.querySelector('body')!
 const keydown$ = fromEvent(ele, 'keydown') as Observable<KeyboardEvent>
-keydown$.pipe(
+const keycode$ = keydown$.pipe(
     // should use pluck, but pluck lacks type info
-    map(i => i.key),
-    windowCount(PATTERN.length, 1),
-    map(stream => stream.pipe(
+    map(e => e.key),
+    share()
+)
+
+keycode$.pipe(
+    mergeMap(code => keycode$.pipe(
+        take(PATTERN.length - 1),
         // it is a cold observable, so will only begin time count when subscribe
         takeUntil(timer$),
         reduce((acc: string[], val: string) => {
             acc.push(val)
             return acc
-        }, [] as string[]),
-    )),
-    mergeAll()
+        }, [code])
+    ))
 ).subscribe(keys => {
     if (arrayEqual(keys, PATTERN)) {
         console.log('%cYay!', 'color: red; font-size: 16px; font-weight: bold')
