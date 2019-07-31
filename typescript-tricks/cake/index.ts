9f427f5d07858e7674cccc546a014da9717bcf85
austaras <austaras@outlook.com>
2019-08-01 01:42:11 +0800

    add drop down

--- typescript-tricks/cake/index.ts
+++ typescript-tricks/cake/index.ts
@@ -1,43 +1,44 @@
 type TupleToUnion<T extends any[]> = T[number]
 // here be dragons!
-type UnionToIntersection<U> =
-    (U extends any ? (k: U) => void : never) extends (k: infer I) => void ? I : never
+type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends (k: infer I) => void
+    ? I
+    : never
 // multiple candidates for the same type variable in contra-variant positions,
 // e.g., function arguments, causes an intersection type to be inferred
 // but bare type parameters before extends in a conditional type are distributed
 // across any union constituents, so we need to pack it into a function
 
-export type test = UnionToIntersection<1 | 2 | 3 | 4> // 1 & 2 & 3 & 4
+export type SomeLongNameType = UnionToIntersection<1 | 2 | 3 | 4> // 1 & 2 & 3 & 4
 
 type Methods<T> = Record<string, (this: T) => any>[]
 
-export function CakeFactory<
-    T extends Record<string, any>,
-    R extends Methods<T>>(
-    data: T, ...fns: R):
-    T & UnionToIntersection<TupleToUnion<R>> {
+export function CakeFactory<T extends Record<string, any>, R extends Methods<T>>(
+    data: T,
+    ...fns: R
+): T & UnionToIntersection<TupleToUnion<R>> {
     const res = Object.assign(data)
-    fns.forEach(fn => Object.entries(fn).forEach(([key, func]) => res[key] = func))
+    fns.forEach(fn => Object.entries(fn).forEach(([key, func]) => (res[key] = func)))
     return res
 }
 
-const batman = CakeFactory({
-    name: 'Batman',
-    secertId: 'Bruce Wayne'
-},
-{
-    fly() {
-        console.log('Bat Wings!')
+const batman = CakeFactory(
+    {
+        name: 'Batman',
+        secertId: 'Bruce Wayne'
     },
-    isRich() {
-        return true
+    {
+        fly() {
+            console.log('Bat Wings!')
+        },
+        isRich() {
+            return true
+        }
+    },
+    {
+        why() {
+            console.log(`Beacuse I'm ${this.name}`)
+        }
     }
-},
-{
-    why() {
-        console.log(`Beacuse I'm ${this.name}`)
-    }
-}
 )
 
 batman.why()
