39f17f457d519356ab63b682e13c6e501e172f26
austaras <austaras@outlook.com>
2019-08-10 01:38:15 +0800

    minor change

--- typescript-tricks/middleware/index.ts
+++ typescript-tricks/middleware/index.ts
@@ -2,7 +2,8 @@ import { Handler } from './handler'
 import { doNothingMiddleware, logMiddleware, thanosMiddleware, makeAddMiddleware } from './middlewares'
 
 const handler = new Handler()
-handler.applyMiddleware(thanosMiddleware, makeAddMiddleware('y', 1), doNothingMiddleware, logMiddleware)
+const addYMiddleware = makeAddMiddleware('y', 1)
+handler.applyMiddleware(thanosMiddleware, addYMiddleware, doNothingMiddleware, logMiddleware)
 const payload = { x: 1 }
 for (let i = 0; i < 10; i++) {
     handler.handle(payload)
