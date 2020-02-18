f839dc72fa2e5a9bf1651c0f4396976bfc6a6a98
austaras <austaras@outlook.com>
2020-02-18 09:23:18 +0800

    less noisy

--- typescript-tricks/zcombinator/index.ts
+++ typescript-tricks/zcombinator/index.ts
@@ -1,23 +1,29 @@
+// normal recursion
 const f = (n: number): number => (n === 0 ? 1 : n * f(n - 1))
 console.log(f(10))
 
 type FA = (f: FA) => (n: number) => number
-const fa = (f: FA) => (n: number) => (n === 0 ? 1 : n * f(f)(n - 1))
+// get recurse body
+const fa: FA = f => n => (n === 0 ? 1 : n * f(f)(n - 1))
 const F1 = fa(fa)
 console.log(F1(10))
 
 type G = (n: number) => number
-const fb = (f: FA) => ((g: G) => (n: number) => (n === 0 ? 1 : n * g(n - 1)))(n => f(f)(n))
+// lift f(f) out, js is eager so use η-reduction
+type FB = (g: G) => G
+const fb: FA = f => ((g => n => (n === 0 ? 1 : n * g(n - 1))) as FB)(n => f(f)(n))
 const F2 = fb(fb)
 console.log(F2(10))
 
-const ga = (g: G) => (n: number) => (n === 0 ? 1 : n * g(n - 1))
-type GA = typeof ga
-const fg = (g: GA) => (f: FA) => g(n => f(f)(n))
+// lift concrete logic out
+const ga: FB = g => n => (n === 0 ? 1 : n * g(n - 1))
+type FG = (g: FB) => (f: FA) => G
+const fg: FG = g => f => g(n => f(f)(n))
 const FG = fg(ga)(fg(ga))
 console.log(FG(10))
 
-// λf. (λx.λv. f(x x v))(λx. f(x x v))
-const Z = (g: GA) => ((f: FA) => g(n => f(f)(n)))(f => g(n => f(f)(n)))
-const FZ = Z(ga), FZ1 = ga(Z(ga)) // ZF = F(ZF)
-console.log(FZ(10), FZ1(10))
+// λf. (λx.λv. f(x x v))(λx.λv. f(x x v))
+const Z = (g: FB) => ((f: FA) => g(n => f(f)(n)))(f => g(n => f(f)(n)))
+const ZF = Z(ga),
+    FZF = ga(Z(ga))
+console.log(ZF(10), FZF(10))
