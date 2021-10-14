045df1e11950976d3a7d9acd616de975b23ac149
austaras <austaras@outlook.com>
2021-10-14 17:01:40 +0800

    parcel 2

--- typescript-tricks/async-calc/async-calculation.ts
+++ typescript-tricks/async-calc/async-calculation.ts
@@ -1,101 +1,98 @@
 interface Task {
-    data: Record<string, any>
-    resolve: Function
-    reject: Function
+  data: Record<string, any>
+  resolve: Function
+  reject: Function
 }
 
 type Copyable =
-    | number
-    | string
-    | boolean
-    | undefined
-    | null
-    | void
-    | Date
-    | Blob
-    | RegExp
-    | { [key: string]: Copyable }
-    | Copyable[]
-    | Map<Copyable, Copyable>
-    | Set<Copyable>
+  | number
+  | string
+  | boolean
+  | undefined
+  | null
+  | void
+  | Date
+  | Blob
+  | RegExp
+  | { [key: string]: Copyable }
+  | Copyable[]
+  | Map<Copyable, Copyable>
+  | Set<Copyable>
 
 class AsyncWorker {
-    private worker = new Worker('./worker.ts')
-    private task?: Task
-    public idle = true
-    constructor(private queue: Task[]) {
-        this.worker.onmessage = event => {
-            if (this.task) {
-                this.task.resolve(event.data)
-                this.final()
-            }
-        }
-        this.worker.onerror = event => {
-            if (this.task) {
-                this.task.reject(event)
-                this.final()
-            }
-        }
+  private worker = new Worker(new URL('worker.ts', import.meta.url), { type: 'module' })
+  private task?: Task
+  public idle = true
+  constructor(private queue: Task[]) {
+    this.worker.onmessage = event => {
+      if (this.task) {
+        this.task.resolve(event.data)
+        this.final()
+      }
     }
-    private final() {
-        this.idle = true
-        this.task = undefined
-        // check if has pending tasks
-        this.work()
+    this.worker.onerror = event => {
+      if (this.task) {
+        this.task.reject(event)
+        this.final()
+      }
     }
-    public work() {
-        if (this.queue.length !== 0) {
-            this.idle = false
-            const work = this.queue.shift()!
-            this.task = work
-            this.worker.postMessage(work.data)
-        }
+  }
+  private final() {
+    this.idle = true
+    this.task = undefined
+    // check if has pending tasks
+    this.work()
+  }
+  public work() {
+    if (this.queue.length !== 0) {
+      this.idle = false
+      const work = this.queue.shift()!
+      this.task = work
+      this.worker.postMessage(work.data)
     }
+  }
 }
 export class AsyncCalculation {
-    private workers: AsyncWorker[]
-    // because tasks are finished in uncertain order
-    private queue: Task[] = []
-    constructor(
-        private concurrency: number // because I can
-    ) {
-        if (concurrency < 1) {
-            throw new Error('worker number cannot be less than one')
-        }
-        this.workers = [new AsyncWorker(this.queue)]
+  private workers: AsyncWorker[]
+  // because tasks are finished in uncertain order
+  private queue: Task[] = []
+  constructor(
+    private concurrency: number // because I can
+  ) {
+    if (concurrency < 1) {
+      throw new Error('worker number cannot be less than one')
     }
-    private dispatch(data: Record<string, any>) {
-        const promise = new Promise((resolve, reject) => {
-            this.queue.push({
-                data,
-                reject,
-                resolve
-            })
-        })
-        let worker = this.workers.find(worker => worker.idle)
-        if (worker !== undefined) {
-            worker.work()
-            return promise
-        }
-        if (this.workers.length < this.concurrency) {
-            // then check if worker number has reached limit
-            // if no, create new worker and use it
-            worker = new AsyncWorker(this.queue)
-            this.workers.push(worker)
-            worker.work()
-            return promise
-        }
-        // wait untill some worker picked this work
-        return promise
+    this.workers = [new AsyncWorker(this.queue)]
+  }
+  private dispatch(data: Record<string, any>) {
+    const promise = new Promise((resolve, reject) => {
+      this.queue.push({
+        data,
+        reject,
+        resolve
+      })
+    })
+    let worker = this.workers.find(worker => worker.idle)
+    if (worker !== undefined) {
+      worker.work()
+      return promise
     }
-    public async calc<T extends Copyable[], R extends Copyable>(
-        func: (this: undefined, ...args: T) => R,
-        ...args: T
-    ): Promise<R> {
-        return this.dispatch({
-            funcStr: func.toString(),
-            name: func.name,
-            args
-        }) as Promise<R>
+    if (this.workers.length < this.concurrency) {
+      // then check if worker number has reached limit
+      // if no, create new worker and use it
+      worker = new AsyncWorker(this.queue)
+      this.workers.push(worker)
+      worker.work()
+      return promise
     }
+    // wait untill some worker picked this work
+    return promise
+  }
+  public async calc<T extends Copyable[], R extends Copyable>(func: (this: undefined, ...args: T) => R, ...args: T): Promise<R> {
+    return this.dispatch({
+      funcStr: func.toString(),
+      name: func.name,
+      args
+    }) as Promise<R>
+  }
 }
--- typescript-tricks/async-calc/index.ts
+++ typescript-tricks/async-calc/index.ts
@@ -3,25 +3,22 @@ import { AsyncCalculation } from './async-calculation'
 const asyncCalc = new AsyncCalculation(2)
 
 function fib(n: number): number {
-    return (n === 0 || n === 1) ?
-        1 : fib(n - 1) + fib(n - 2)
+  return n === 0 || n === 1 ? 1 : fib(n - 1) + fib(n - 2)
 }
 
-asyncCalc.calc(fib, 40).then(
-    res => console.log('fib, may take long time', res)
-)
+asyncCalc.calc(fib, 40).then(res => console.log('fib, may take long time', res))
 
 // four ways of define function
 function add0(a: number, b: number) {
-    return a + b
+  return a + b
 }
-const add1 = function(a: number, b: number) {
-    return a + b
+const add1 = function (a: number, b: number) {
+  return a + b
 }
 const add2 = (a: number, b: number) => a + b
 // eslint-disable-next-line
 const add3 = (a: number, b: number) => {
-    return a + b
+  return a + b
 }
 
 asyncCalc.calc(add0, 1, 2).then(res => console.log('add0', res))
@@ -31,23 +28,23 @@ asyncCalc.calc(add3, 1, 2).then(res => console.log('add3', res))
 
 // @ts-ignore: noImplicitAny
 const square = x => x * x
-// eslint-disable-next-line
-const mislead = () => { x: 1 }
+const mislead = () => {
+  // eslint-disable-next-line
+  x: 1
+}
 const empty = () => ({})
 asyncCalc.calc(square, 50).then(res => console.log('square', res))
-asyncCalc.calc(mislead).then(
-    res => console.log('mislead, should get undefined', res)
-)
+asyncCalc.calc(mislead).then(res => console.log('mislead, should get undefined', res))
 asyncCalc.calc(empty).then(res => console.log('empty object', res))
 
 function defaultAdd(a: number, b = 1) {
-    return a + b
+  return a + b
 }
 function optionalAdd(a: number, b?: number) {
-    return a + (b ? b : 1)
+  return a + (b ? b : 1)
 }
 function multiAdd(a: number, ...b: number[]) {
-    return a + b.reduce((acc, val) => acc + val, 0)
+  return a + b.reduce((acc, val) => acc + val, 0)
 }
 
 asyncCalc.calc(defaultAdd, 4).then(res => console.log('defalut param', res))
